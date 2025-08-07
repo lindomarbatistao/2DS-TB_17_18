8be8981002d97cbc778315fc90c7802ea877426b
Lindomar <lindomarbatistao@gmail.com>
2025-08-07 16:50:46 -0300

    Tabela Autor

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    data_nasc =  models.DateField(null=True, blank=True)
+    nacion = models.CharField(max_length=30, null=True, blank=True)
+    biogra = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+
---
+++ api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Autor
+
+class AutorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = '__all__'
