17c48db4d7c1fd306707b79378f7df032afa386f
Lindomar <lindomarbatistao@gmail.com>
2025-09-04 13:07:50 -0300

    X

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     # GET / POST
@@ -7,10 +12,15 @@ urlpatterns = [
     path('autores', AutoresView.as_view()), 
     path('editoras', EditorasView.as_view()),
     path('livros', LivrosView.as_view()),
+    path('buscar/', AutoresView.as_view()),
     
     # UPDATE / DELETE
     path('autor/<int:pk>', AutoresDetailView.as_view()), 
     path('editora/<int:pk>', EditorasDetailView.as_view()),
     path('livro/<int:pk>', LivrosDetailView.as_view()),
+
+    # TOKEN
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
